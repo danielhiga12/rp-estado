38ba0a4e910f86f7d6d9b51d3f5262c75978a269
danielhiga12 <danielhiga121@gmail.com>
2026-01-07 02:47:39 +0900

    Update server.js

--- src/api/server.js
+++ src/api/server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>LAGUNA ROLEPLAY — Estado RP Online</h1>");
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Site institucional online"));
 
