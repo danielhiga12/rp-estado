a303baf6dc6e49346ad6b64dfb5c14de98a5ece5
danielhiga12 <danielhiga121@gmail.com>
2026-01-07 02:55:28 +0900

    Update setcnh.js

--- src/bot/commands/setcnh.js
+++ src/bot/commands/setcnh.js
@@ -1,1 +1,24 @@
+const fs = require("fs");
+const path = "./src/data/cnhs.json";
+let cnhs = {};
+if (fs.existsSync(path)) cnhs = JSON.parse(fs.readFileSync(path));
+
+module.exports = {
+  name: "setcnh",
+  description: "Cadastra CNH (apenas cargos permitidos)",
+  async execute(interaction) {
+    const cargoPermitido = ["Polícia Militar","Polícia Civil","PRF","PF","Polícia do Exército"];
+    const userCargo = interaction.options.getString("cargo");
+    if (!cargoPermitido.includes(userCargo)) return interaction.reply("Você não tem permissão para cadastrar CNH.");
+
+    const categoria = interaction.options.getString("categoria");
+    const emissao = interaction.options.getString("emissao");
+    const validade = interaction.options.getString("validade");
+
+    cnhs[interaction.user.id] = { categoria, emissao, validade, cargoPermitido: userCargo };
+    fs.writeFileSync(path, JSON.stringify(cnhs, null, 2));
+
+    interaction.reply(`CNH cadastrada!\nCategoria: ${categoria}\nEmissão: ${emissao}\nValidade: ${validade}\nCargo: ${userCargo}`);
+  }
+};
 
